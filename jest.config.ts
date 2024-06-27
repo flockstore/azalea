206a60a1ff934aaafe2b3cdecd70aa82ce353203
Ian Castaño <ianfe@martina.com.co>
2024-06-26 21:54:45 -0500

    feat: Added testing tools

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import nextJest from "next/jest.js";
+import {Config} from "@jest/types";
+
+const createJestConfig = nextJest({
+    dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config: Config.InitialOptions = {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts",],
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        ".*test-util\\.(jsx?|tsx?)$"
+    ],
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
+};
+
+export default createJestConfig(config);
