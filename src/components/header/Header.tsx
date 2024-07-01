0275a14a4ddb5e4f8a7edc5ddf5852cff9e5d308
Ian Castaño <ianfe@martina.com.co>
2024-06-30 19:28:28 -0500

    feat: Added toolbar dummy

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,22 @@
 import {Flex} from "@mantine/core";
 import HeaderBreadcrumb from "@/components/header/partial/breadcrumb/HeaderBreadcrumb";
+import HeaderToolbar from "@/components/header/partial/toolbar/HeaderToolbar";
+import {useSidebar} from "@/context/sidebar/SidebarContext";
 
 const Header = () => {
+
+    const { canCollapse } = useSidebar();
+
     return (
         <Flex
             component="header"
             my="xl"
             w="100%"
+            justify="space-between"
+            align="center"
         >
             <HeaderBreadcrumb/>
+            {canCollapse() && <HeaderToolbar/>}
         </Flex>
     );
 };
