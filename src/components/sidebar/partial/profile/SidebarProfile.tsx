b97a618cacbd4775cb0519780ddcfe6487c222ad
Ian Castaño <ianfe@martina.com.co>
2024-07-01 18:47:22 -0500

    feat: Ready por unit test

--- src/components/sidebar/partial/profile/SidebarProfile.tsx
+++ src/components/sidebar/partial/profile/SidebarProfile.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import {ActionIcon, Avatar, Flex, Text, Tooltip} from "@mantine/core";
+import {ActionIcon, Avatar, Flex, Skeleton, Text, Tooltip} from "@mantine/core";
 import {IconLogout} from "@tabler/icons-react";
 
 import styles from "./SidebarProfile.module.css";
 import {useSidebar} from "@/context/sidebar/SidebarContext";
 import {useTranslations} from "next-intl";
-import {profile} from "@/config/translation";
-import {signOut} from "next-auth/react";
+import {signOut, useSession} from "next-auth/react";
 import {useRouter} from "next/navigation";
+import {User} from "next-auth";
+import {profile} from "@/config/translation";
 
 const SidebarProfile = () => {
 
     const { isExpanded } = useSidebar();
     const t = useTranslations();
     const router = useRouter();
+    const {data: session, status} = useSession();
+    const userProfile = session?.user as User;
 
     const profileStyles = isExpanded() ? styles.profileExpanded : styles.profile;
 
@@ -28,45 +31,47 @@ const SidebarProfile = () => {
     };
 
     return (
-        <Flex className={profileStyles}>
+        <Skeleton visible={status === "loading"}>
+            <Flex className={profileStyles}>
 
-            <Flex
-                align="center"
-                className={`${!isExpanded() && styles.slug}`}
-            >
-                <Tooltip
-                    label={gagName}
-                    position={profileTooltipPosition}
-                    offset={10}
+                <Flex
+                    align="center"
+                    className={`${!isExpanded() && styles.slug}`}
                 >
-                    <Avatar
-                        className={styles.picture}
-                        src="img/avatar-holder.webp"
-                        alt="Avatar"
-                    />
-                </Tooltip>
-                {isExpanded() &&
-                    <Flex className={styles.context}>
-                        <Text className={styles.name}>Ian Felipe</Text>
-                        <Text className={styles.organization}>Flock Store</Text>
-                    </Flex>
-                }
+                    <Tooltip
+                        label={gagName}
+                        position={profileTooltipPosition}
+                        offset={10}
+                    >
+                        <Avatar
+                            className={styles.picture}
+                            src={userProfile.image || "img/avatar-holder.webp"}
+                            alt={userProfile.name}
+                        />
+                    </Tooltip>
+                    {isExpanded() &&
+                        <Flex className={styles.context}>
+                            <Text className={styles.name}>{userProfile.name}</Text>
+                            <Text className={styles.organization}>{userProfile.organizations[0] || ''}</Text>
+                        </Flex>
+                    }
+                </Flex>
+
+                <Flex>
+                    <Tooltip
+                        label={t(profile.logout)}
+                        position="right"
+                        offset={10}
+
+                    >
+                        <ActionIcon className={styles.logout} onClick={logout}>
+                            <IconLogout/>
+                        </ActionIcon>
+                    </Tooltip>
+                </Flex>
+
             </Flex>
-
-            <Flex>
-                <Tooltip
-                    label={t(profile.logout)}
-                    position="right"
-                    offset={10}
-
-                >
-                    <ActionIcon className={styles.logout} onClick={logout}>
-                        <IconLogout/>
-                    </ActionIcon>
-                </Tooltip>
-            </Flex>
-
-        </Flex>
+        </Skeleton>
     );
 };
 
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,20 +1,29 @@
 declare module "next-auth" {
     import {DefaultSession, DefaultUser} from "next-auth";
 
-    interface Session {
+    interface Session extends DefaultSession {
         user: {
-            organizations?: string[];
-            custom_data?: Record<string, any>;
+            name: string;
+            email: string;
+            image: string;
+            organizations: string[];
+            custom_data: Record<string, any>;
         } & DefaultSession["user"];
     }
 
     interface User extends DefaultUser {
-        organizations?: string[];
-        custom_data?: Record<string, any>;
+        name: string;
+        email: string;
+        image: string;
+        organizations: string[];
+        custom_data: Record<string, any>;
     }
 
     interface JWT {
-        organizations?: string[];
-        custom_data?: Record<string, any>;
+        name: string;
+        email: string;
+        image: string;
+        organizations: string[];
+        custom_data: Record<string, any>;
     }
 }
