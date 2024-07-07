f41fa70e7b87898a2a991ca722197a6904ea2344
Ian Castaño <ianfe@martina.com.co>
2024-07-07 14:57:32 -0500

    feat: added login header

--- src/layout/login/LoginLayout.tsx
+++ src/layout/login/LoginLayout.tsx
@@ -3,6 +3,7 @@ import {Flex, Grid} from "@mantine/core";
 
 import styles from "./LoginLayout.module.css";
 import LoginPicture from "@/layout/login/components/picture/LoginPicture";
+import LoginHeader from "@/layout/login/components/header/LoginHeader";
 
 /**
  * Defines the rendering behaviour of the dashboard
@@ -14,7 +15,10 @@ const LoginLayout = ({children}: { children: React.ReactNode }) => {
     return (
         <Flex className={styles.layout}>
             <Flex className={styles.pictureCol}><LoginPicture/></Flex>
-            <Flex>{children}</Flex>
+            <Flex className={styles.main}>
+                <LoginHeader/>
+                {children}
+            </Flex>
         </Flex>
     );
 };
---
+++ src/layout/login/components/header/LoginHeader.tsx
@@ -0,0 +1,32 @@
+ "use client";
+
+import {Flex, useMantineColorScheme, useMantineTheme} from "@mantine/core";
+import Logo from "@/components/logo/Logo";
+import SidebarSwitcher from "@/components/sidebar/partial/switcher/SidebarSwitcher";
+
+import styles from "./LoginHeader.module.css";
+
+const LoginHeader = () => {
+
+    const {colorScheme} = useMantineColorScheme();
+    const {colors} = useMantineTheme();
+    const logoLight = colors["azalea-blue"][4];
+    const logoDark = colors.dark[3];
+    const logo = colorScheme === "dark" ? logoDark : logoLight;
+
+    const width = 42;
+
+    return (
+        <Flex className={styles.header}>
+            <Flex className={styles.logo}>
+                <Logo
+                    color={logo}
+                    collapsed={false}
+                />
+            </Flex>
+            <SidebarSwitcher width={width}/>
+        </Flex>
+    );
+};
+
+export default LoginHeader;
